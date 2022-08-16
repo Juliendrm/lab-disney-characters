6561ba34c1d33b64678e638c3093505ed8477b6a
Julien <benedetjulien@gmail.com>
2022-08-16 17:12:44 +0200

    Solved lab, not bonus

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-/**
- *  Your code ⬇️
- */
+const express = require("express");
+const port = 3000;
+const app = express();
+const jsonDisney = require("./disney.json");
+app.use(express.json()); //
+
+//function pour choper les characters par name.
+app.get("/characters", (req, res) => {
+  //res.send(jsonDisney).
+  const name = req.query.name;
+  if (name) {
+    const result = jsonDisney.filter((oneCharacter) => {
+      return oneCharacter.name.includes(name);
+    });
+    return res.json({
+      message: `Found ${result.length} characters(s)`,
+      result,
+    });
+  }
+  res.json(jsonDisney); // renvoie tout le fichier si pas de query.
+});
+
+app.get("/characters/:_id", (req, res) => {
+  const { _id } = req.params; // params est comme query mais permet de recuperer les trucs dans l url precedes par ':' il renvoie en objet la clef _id et la valeur sera le numero dans l url.
+  const uniqueCharacter = jsonDisney.find((character) => {
+    return character._id === Number(_id); // attention a convertir la string en number.
+  });
+  return res.json({
+    message: ` We found ${uniqueCharacter.name} !`,
+    uniqueCharacter,
+  });
+});
+
+app.post("/characters", (req, res) => {
+  let [{ name, films }] = req.body; // take what they give
+  _id = jsonDisney.length + 1;
+  const characterToCreate = { name, films, _id };
+  jsonDisney.push(characterToCreate); // push in database
+});
+
+app.listen(port, () => {
+  console.log("I am listening http://localhost:3000");
+});
